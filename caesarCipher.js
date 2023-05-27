0b3d0ae015e7f502faa1f77639881707d0858887
chucoyos <unodev@outlook.com>
2023-05-27 15:25:59 -0600

    Add caesarCipher test

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,11 +1,27 @@
 function caesarCipher(str, shiftBy) {
-  let result = ''
+  // create a function that takes a string and a number
+  // shift each letter in the string by the number
+  // wrap around the alphabet if necessary
+  // keep the same case and punctuation
+  // return the new string
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseAlphabet = alphabet.toUpperCase()
+  let newStr = ''
   for (let i = 0; i < str.length; i++) {
-    let charCode = str.charCodeAt(i)
-    let newCharCode = charCode + shiftBy
-    result += String.fromCharCode(newCharCode)
+    if (alphabet.includes(str[i])) {
+      let index = alphabet.indexOf(str[i])
+      let newIndex = (index + shiftBy) % 26
+      newStr += alphabet[newIndex]
+    } else if (upperCaseAlphabet.includes(str[i])) {
+      let index = upperCaseAlphabet.indexOf(str[i])
+      let newIndex = (index + shiftBy) % 26
+      newStr += upperCaseAlphabet[newIndex]
+    } else {
+      newStr += str[i]
+    }
   }
-  return result
+  return newStr
+
 }
 
 export default caesarCipher
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,7 +1,28 @@
 import caesarCipher from "./caesarCipher";
 
 describe('caesarCipher shift', () => {
-  it('Returns a Caesar shifted string', () => {
+  it('Returns a Caesar shifted string by 1', () => {
     expect((caesarCipher('hello', 1))).toBe('ifmmp')
+  })
+  it('Returns a Caesar shifted string by 3', () => {
+    expect((caesarCipher('hello', 3))).toBe('khoor')
+  })
+  it('Returns a Caesar shifted string by 5', () => {
+    expect((caesarCipher('hello', 5))).toBe('mjqqt')
+  })
+  it('Returns a Caesar shifted string by 6', () => {
+    expect((caesarCipher('zoop', 6))).toBe('fuuv')
+  })
+  it('Returns a Caesar shifted string by 26', () => {
+    expect((caesarCipher('hello', 26))).toBe('hello')
+  })
+  it('Returns a Caesar shifted string by 27', () => {
+    expect((caesarCipher('hello', 27))).toBe('ifmmp')
+  })
+  it('Returns a Caesar shifted string by 6 with punctuation', () => {
+    expect((caesarCipher('hello!', 6))).toBe('nkrru!')
+  })
+  it('Returns a Caesar shifted string by 6 with capital letters', () => {
+    expect((caesarCipher('Hello!', 6))).toBe('Nkrru!')
   })
 })
