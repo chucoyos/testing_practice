df5be030c3788505278fd3edcd60277c609c8259
chucoyos <unodev@outlook.com>
2023-05-27 23:30:24 -0600

    Add more tests to calculator test suite

--- calculator.js
+++ calculator.js
@@ -1,15 +1,16 @@
 const calculator = (function() {
   const add = (a, b) => {
-    return a + b
+    // calculates the sum of two numbers including negative numbers and strings
+    return Number(a) + Number(b)
   }
   const subtract = ((a, b) => {
-    return a - b
+    return Number(a) - Number(b)
   })
   const divide = (a, b) => {
-    return a / b
+    return Number(a) / Number(b)
   }
   const multiply = (a, b) => {
-    return a * b
+    return Number(a) * Number(b)
   }
   return {
     add,
--- calculator.test.js
+++ calculator.test.js
@@ -4,13 +4,34 @@ describe('calculator module', () => {
   it('calculates the sum of two numbers', () => {
     expect((calculator.add(2, 4))).toBe(6)
   })
+  it('calculates the sum of two numbers with negative numbers', () => {
+    expect((calculator.add(-2, 4))).toBe(2)
+  })
+  it('calculates the sum of two numbers with strings', () => {
+    expect((calculator.add('2', '4'))).toBe(6)
+  })
   it('calculates the subtraction of two numbers', () => {
     expect((calculator.subtract(8, 4))).toBe(4)
+  })
+  it('calculates the subtraction of two numbers with negative numbers', () => {
+    expect((calculator.subtract(-8, 4))).toBe(-12)
+  })
+  it('calculates the subtraction of two numbers with strings', () => {
+    expect((calculator.subtract('8', '4'))).toBe(4)
   })
   it('calculates the division of two numbers', () => {
     expect((calculator.divide(8, 4))).toBe(2)
   })
+  it('calculates the division of two numbers with negative numbers', () => {
+    expect((calculator.divide(-8, 4))).toBe(-2)
+  })
+  it('calculates the division of two numbers with strings', () => {
+    expect((calculator.divide('8', '4'))).toBe(2)
+  })
   it('calculates the multiplication of two numbers', () => {
     expect((calculator.multiply(4, 5))).toBe(20)
+  })
+  it('calculates the multiplication of two numbers with negative numbers', () => {
+    expect((calculator.multiply(-4, 5))).toBe(-20)
   })
 })
