8ef0ee623f2b0e6824d8b49e42b601170edd772a
chucoyos <unodev@outlook.com>
2023-05-27 23:18:35 -0600

    Add more tests to reverseString test suite

--- reverseString.test.js
+++ reverseString.test.js
@@ -1,4 +1,18 @@
 import reverseString from "./reverseString"
-it('returns a reversed string', () => {
-  expect(reverseString('huston')).toBe('notsuh')
+describe('reverseString', () => {
+  it('returns a reversed string', () => {
+    expect(reverseString('huston')).toBe('notsuh')
+  })
+  it('returns a reversed string', () => {
+    expect(reverseString('zoom')).toBe('mooz')
+  })
+  it('returns a reversed string with spaces', () => {
+    expect(reverseString('zoom zoom')).toBe('mooz mooz')
+  })
+  it('returns a reversed string with numbers', () => {
+    expect(reverseString('123')).toBe('321')
+  })
+  it('returns a reversed string with special characters', () => {
+    expect(reverseString('!@#$')).toBe('$#@!')
+  })
 })
